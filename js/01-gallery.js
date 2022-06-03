2153e195e19c3bd482dc74f70f32d5b6ed90f54c
DariaOdessa <dariaonlynew@gmail.com>
2022-06-03 17:52:40 +0300

    add Homework-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+createGalleryItem(galleryItems);
+ 
+function createGalleryItem() {
+    const markup = galleryItems.map(({ preview, original, description })=>
+  `<div class="gallery__item">
+      <a class="gallery__link" href=${original}>
+      <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt='${description}'
+      />
+    </a>
+  </div>`).join('');
+    
+  galleryRef.innerHTML = markup; 
+};
+
+galleryRef.addEventListener('click', onSelectedPicture);
+
+let modalWindow;
+
+function onSelectedPicture(event) {
+    event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const largeImage = event.target.dataset.source; 
+  modalWindow = basicLightbox.create(`<img src=${largeImage} width="800" height="600"> `, {
+    onShow: () => window.addEventListener('keydown', closeModalOnEscKey),
+    onClose: () => window.addEventListener('keydown', closeModalOnEscKey),
+  });
+  modalWindow.show();
+};
+
+function closeModalOnEscKey(event) {
+  if (event.code === 'Escape') {
+    modalWindow.close();
+  }
+};
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+createGalleryItem(galleryItems);
+ 
+function createGalleryItem() {
+    const markup = galleryItems.map(({ preview, original, description })=>
+        `<a class="gallery__item" 
+        href=${original}>
+        <img class="gallery__image"
+            src=${preview}
+            alt='${description}' />
+        </a>`).join('');
+    
+  galleryRef.innerHTML = markup; 
+};
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    // preloading: false,
+    // alertError: false
+    
+});
+
+
+
+
+
+
